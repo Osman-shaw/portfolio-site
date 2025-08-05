7f0cf07fbae6e9543e44588aa419160890cd98a0
Osman Shaw <shawosman090@gmail.com>
2025-08-06 00:44:18 +0100

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig({
   server: {
     // eslint-disable-next-line no-undef
     port: process.env.PORT || 3000, // Use PORT from .env or default to 3000
+    build: {
+    outDir: 'dist', // Build output directory
+  },
    
   }
 })
